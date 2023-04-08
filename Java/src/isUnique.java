ed5b676eafbfc344f36d828737bb3d7adea221a4
prateek041 <prateeksingh9741@gmail.com>
2023-04-08 23:43:23 +0530

    Improved: isUnique

--- Java/src/isUnique.java
+++ Java/src/isUnique.java
@@ -1,10 +1,20 @@
+import java.util.HashSet;
+
 public class isUnique {
     public static void main(String[] args){
-        String word = "Heloe";
+        String word = "Helo";
         int length = word.length();
-        System.out.println(isUniqueBrute(word, length));
+        if (length == 0){
+            System.out.println("true");
+
+        }
+        else {
+//            System.out.println(isUniqueBrute(word, length));
+            System.out.println(isUniqueHash(word, length));
+        }
     }
 
+    // This made it O(n2)
     public static Boolean isUniqueBrute(String word, int length){
         if (length <= 0){
             return false;
@@ -16,6 +26,20 @@ public class isUnique {
                     return false;
                 }
             }
+        }
+        return true;
+    }
+
+    // This made it O(n)
+    public static Boolean isUniqueHash(String word, int length){
+        int [] test = {1,2,3,4};
+        System.out.println(test.length);
+        HashSet<Character> map = new HashSet<>(); // creating a new hashset
+        for(int i = 0; i<length; i++){
+            if (map.contains(word.charAt(i))){
+                return false;
+            }
+            map.add(word.charAt(i));
         }
         return true;
     }
---
+++ Java/src/learning.md
@@ -0,0 +1,8 @@
+# Collection of what I have learnt
+
+# isUnique
+
+- String.charAt(index) can be used to get character at index "index" in the string.
+- string.length() works with 1 index exactly like array.length.
+- map.Contains(value) is used to check if something exists into the map.
+- map.add(value) is used to put something inside the map.
