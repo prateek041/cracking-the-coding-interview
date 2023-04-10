d6eb3295cc20998467a05320ebbcee63e86c537f
prateek041 <prateeksingh9741@gmail.com>
2023-04-10 11:24:49 +0530

    Update: isUnique [Java]

--- Java/src/isUnique.java
+++ Java/src/isUnique.java
@@ -2,19 +2,20 @@ import java.util.HashSet;
 
 public class isUnique {
     public static void main(String[] args){
-        String word = "Helo";
+        String word = "Hello";
         int length = word.length();
         if (length == 0){
             System.out.println("true");
 
         }
         else {
-//            System.out.println(isUniqueBrute(word, length));
-            System.out.println(isUniqueHash(word, length));
+            // System.out.println(isUniqueBrute(word, length));
+            // System.out.println(isUniqueHash(word, length));
+            System.out.println(isUniqueArray(word, length));
         }
     }
 
-    // This made it O(n2)
+    // This made it O(n2). Since it is the brute force technique.
     public static Boolean isUniqueBrute(String word, int length){
         if (length <= 0){
             return false;
@@ -30,7 +31,7 @@ public class isUnique {
         return true;
     }
 
-    // This made it O(n)
+    // This made it O(n) since I am using hashset. But the lookup time is still not always constant.
     public static Boolean isUniqueHash(String word, int length){
         int [] test = {1,2,3,4};
         System.out.println(test.length);
@@ -40,6 +41,30 @@ public class isUnique {
                 return false;
             }
             map.add(word.charAt(i));
+        }
+        return true;
+    }
+
+    // Using the array now. This will make the lookup time constant.
+    public static boolean isUniqueArray(String word, int length){
+        // Let's consider that the string is using Unicode, hence only 256 characters, otherwise it could be 128
+        // characters.
+
+        // since there should only be unique characters, length cannot exceed 256.
+        if (length > 256){
+            return false;
+        }
+
+        // create an empty boolean array of length 256
+        boolean [] characters = new boolean[256];
+        int unicodeVal = 0;
+
+        for (int i = 0; i<length; i++){
+            unicodeVal = word.charAt(i);
+            if(characters[unicodeVal]){
+                return false;
+            }
+            characters[unicodeVal] = true;
         }
         return true;
     }
--- Java/src/learning.md
+++ Java/src/learning.md
@@ -1,8 +1,18 @@
 # Collection of what I have learnt
 
-# isUnique
+## isUnique
 
 - String.charAt(index) can be used to get character at index "index" in the string.
-- string.length() works with 1 index exactly like array.length.
-- map.Contains(value) is used to check if something exists into the map.
+- string.length() works with 1 indexing exactly like array.length.
+- map.Contains(value) is used to check if something exists into the map. It is generally O(1) tie but can vary depending
+on the data.
 - map.add(value) is used to put something inside the map.
+- There are two different approaches of solving that problem, one using a Hashset and another using boolean array. 
+Both have their pros and cons.
+
+### Hashset vs Boolean Array:
+- It uses dynamic array, hence less memory usage in case the string contains all the unique characters whereas in 
+  case of Boolean array, doesn't matter if the string is unique or not, it will always allocate same space.
+- On the other hand, the Boolean array works well since it will always only use 256 character values and the lookup 
+  time is constant since we are using indexed, on the other hand, using the hashset might result in non-constant 
+  lookup times.
